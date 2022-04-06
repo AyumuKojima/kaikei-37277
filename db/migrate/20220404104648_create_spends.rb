bd79427afbccbd9a1b159d3268e1064003b05fa7
Ayumu Kojima <youki.6230@gmail.com>
2022-04-06 20:18:49 +0900

    マイグレーションファイルを編集

--- db/migrate/20220404104648_create_spends.rb
+++ db/migrate/20220404104648_create_spends.rb
@@ -1,7 +1,8 @@
 class CreateSpends < ActiveRecord::Migration[6.0]
   def change
     create_table :spends do |t|
-
+      t.string :title, null: false
+      t.references :user, null: false, foreign_key: true
       t.timestamps
       t.integer :money, null: false
       t.date :day, null: false
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_05_114158) do
+ActiveRecord::Schema.define(version: 2022_04_06_111533) do
+
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "spends", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
