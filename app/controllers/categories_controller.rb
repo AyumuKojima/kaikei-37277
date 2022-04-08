e10f0c7cbac30b7ab634145308dc922c1ced3d2d
Ayumu Kojima <youki.6230@gmail.com>
2022-04-09 02:59:23 +0900

    保存したデータ内容をviewに反映

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,5 +2,23 @@ class CategoriesController < ApplicationController
 
   def index
     @categories = Category.all
+    @sum = 0
+    @categories.each do |category|
+      category.spends.each do |spend|
+        @sum += spend.money
+      end
+    end
+    @each_sums = []
+    @categories.each do |category|
+      each_sum = 0
+      category.spends.each do |spend|
+        each_sum += spend.money
+      end
+      @each_sums << each_sum
+    end
+    @each_props = []
+    @each_sums.each do |each_sum|
+      @each_props << (each_sum * 100 / @sum).floor
+    end
   end
 end
