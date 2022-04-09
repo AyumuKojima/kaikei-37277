29aad47d9ac30e5afcb3b424309b2aa6347ae2f7
Ayumu Kojima <youki.6230@gmail.com>
2022-04-09 13:42:29 +0900

    javascriptを編集

--- app/javascript/form-color.js
+++ app/javascript/form-color.js
@@ -6,6 +6,13 @@ function formColor () {
   const categoryNameForm = document.getElementById("category-name-form");
   const categoryColorForm = document.getElementById("category-color-form");
   const categoryInputBtn = document.getElementById("category-input-btn");
+  const num = selector.value;
+  const rgb = colors[num].innerHTML;
+  newCategory.setAttribute("style", `border-color: rgb${rgb};`);
+  newCategoryName.setAttribute("style", `color: rgb${rgb};`);
+  categoryNameForm.setAttribute("style", `border-color: rgb${rgb};`);
+  categoryColorForm.setAttribute("style", `border-color: rgb${rgb};`);
+  categoryInputBtn.setAttribute("style", `background-color: rgb${rgb};`);
 
   selector.addEventListener("change",() => {
     const num = selector.value;
