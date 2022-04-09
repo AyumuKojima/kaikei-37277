f2715f5cd7b3ec86045c9f2d3015043d9e26b17e
Ayumu Kojima <youki.6230@gmail.com>
2022-04-09 15:51:52 +0900

    支出をdbに保存できるように実装

--- app/controllers/spends_controller.rb
+++ app/controllers/spends_controller.rb
@@ -1,5 +1,25 @@
 class SpendsController < ApplicationController
 
   def index
+    fake_category = Category.new(id: 0, title: "カテゴリーを選択してください")
+    @select_categories = [fake_category].push(Category.all).flatten!
+    @spend = Spend.new
+  end
+
+  def create
+    fake_category = Category.new(id: 0, title: "カテゴリーを選択してください")
+    @select_categories = [fake_category].push(Category.all).flatten!
+    @spend = Spend.new(spend_params)
+    if @spend.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def spend_params
+    params.require(:spend).permit(:money, :memo, :category_id, :day).merge(user_id: current_user.id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'spends#index'
-  resources :spends, only: :index
+  resources :spends, only: [:index, :create]
   resources :categories, only: [:index, :create]
 end
