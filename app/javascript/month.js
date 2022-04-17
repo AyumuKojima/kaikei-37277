a8ab10e4f87e37be2e238c8b8c7387b8614136b9
Ayumu Kojima <youki.6230@gmail.com>
2022-04-17 20:12:14 +0900

    編集フォームに自動入力させる処理を実装

--- app/javascript/month.js
+++ app/javascript/month.js
@@ -13,7 +13,30 @@ function month () {
     };
   };
 
-  
+  const showInfos = document.querySelectorAll(".show-info");
+  const spendShowBtns = document.getElementById("spend-show-btns");
+  const yearForm = document.getElementById("_day_1i");
+  const monthForm = document.getElementById("_day_2i");
+  const dayForm = document.getElementById("_day_3i");
+  const showDates = document.querySelectorAll(".show-date");
+  const showSpends = document.querySelectorAll(".show-spend");
+  const moneyForm = document.getElementById("money");
+  const categoryForm = document.getElementById("category_id");
+  const memoForm = document.getElementById("memo");
+  const showMemos = document.querySelectorAll(".show-memo");
+  for (let i=0; i < showInfos.length; i++) {
+    showInfos[i].addEventListener("click", () => {
+      spendShowBtns.setAttribute("style", "display: block;");
+      const date = new Date(showDates[i].innerHTML);
+      yearForm.value = date.getFullYear();
+      monthForm.value = date.getMonth()+1;
+      dayForm.value = date.getDate();
+      moneyForm.value = parseInt(showSpends[i].innerHTML, 10);
+      categoryForm.value = categoryIds[i]+1;
+      memoForm.value = showMemos[i].innerHTML.trim();
+    });
+  };
+
 };
 
 window.addEventListener('turbolinks:load', month);
