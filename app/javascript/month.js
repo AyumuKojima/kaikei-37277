e6c924027641c027aaecabfc245ea3b97410d90c
Ayumu Kojima <youki.6230@gmail.com>
2022-04-23 13:31:31 +0900

    カテゴリー詳細画面から支出の編集・削除ができるようにする

--- app/javascript/month.js
+++ app/javascript/month.js
@@ -46,6 +46,12 @@ function month () {
         setSum(XHR.response.sum);
         updateSpendView(item, categoryMarks[index], showSpends[index], showMemos[index]);
         clearForm(moneyForm, categoryForm, memoForm, updateIdForm, indexForm, yearForm, monthForm, dayForm, spendShowBtns);
+        if (document.getElementById("category-sum") != null){
+          if (XHR.response.past_category_id != item.category_id) {
+            showInfos[index].setAttribute("style", "display: none;");
+          };
+          setCategoryInfo(XHR);
+        };
       } else {
         location.reload();
       };
@@ -54,7 +60,7 @@ function month () {
 
   const spendDeleteBtn = document.getElementById("spend-delete-btn");
   spendDeleteBtn.addEventListener('click', () => {
-    adjustToken()
+    adjustToken();
     const form = document.getElementById("form");
     const formData = new FormData(form);
     XHR = setDeleteXHR(yearForm, monthForm, updateIdForm);
@@ -67,6 +73,10 @@ function month () {
       const index = XHR.response.index;
       showInfos[index].setAttribute("style", "display: none;");
       clearForm(moneyForm, categoryForm, memoForm, updateIdForm, indexForm, yearForm, monthForm, dayForm, spendShowBtns);
+      setSum(XHR.response.sum);
+      if (document.getElementById("category-sum") != null){
+        setCategoryInfo(XHR);
+      };
     };
   });
 };
@@ -130,7 +140,7 @@ function setDeleteXHR (year, month, deleteId) {
 
 function setSum (sum) {
   const monthSum = document.getElementById("month-sum");
-  monthSum.innerHTML = `合計出費額：${sum}円`;
+  monthSum.innerHTML = sum.toLocaleString();
 };
 
 function updateSpendView (item, categoryMark, showSpend, showMemo) {
@@ -151,6 +161,17 @@ function clearForm (moneyForm, categoryForm, memoForm, updateIdForm, indexForm, 
   monthForm.value = today.getMonth()+1;
   dayForm.value = today.getDate();
   btns.setAttribute('style', "display: none;");
+};
+
+function setCategoryInfo (XHR) {
+  const categorySum = document.getElementById("category-sum");
+  const categoryBar = document.getElementById("category-bar");
+  const thinColors = document.querySelectorAll(".thin-color");
+  const colorId = document.getElementById("color-id");
+  const id = colorId.innerHTML;
+  const rgba = thinColors[id].innerHTML;
+  categorySum.innerHTML = `${XHR.response.category_sum}円`
+  categoryBar.setAttribute("style", `background-color: rgba${rgba}; width: ${XHR.response.prop}%;`);
 };
 
 window.addEventListener('turbolinks:load', month);
