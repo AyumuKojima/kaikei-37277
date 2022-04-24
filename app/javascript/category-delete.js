3a6e69fecf00c124db06a289b411d2d8c64d836f
Ayumu Kojima <youki.6230@gmail.com>
2022-04-24 16:52:25 +0900

    削除時のviewを記述

--- app/javascript/category-delete.js
+++ app/javascript/category-delete.js
@@ -2,12 +2,14 @@ function categoryDelete () {
   const deleteBtn = document.getElementById("category-delete-btn");
   deleteBtn.addEventListener('click', () => {
     const showList = document.getElementById("show-list");
-    const destroyMessage = document.getElementById("destroy-message");
+    const destroyMessageBox = document.getElementById("destroy-message-box");
     const subContents = document.getElementById("sub-contents");
+    const categoryBtns = document.getElementById("category-btns");
 
     showList.setAttribute('class', "show-list hidden");
-    destroyMessage.setAttribute('class', "destroy-message");
+    destroyMessageBox.setAttribute('class', "destroy-message-box");
     subContents.setAttribute('class', "sub-contents hidden");
+    categoryBtns.setAttribute('style', 'display: none;');
   });
 };
 
