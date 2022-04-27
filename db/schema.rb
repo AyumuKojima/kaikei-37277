2d7b946f3f76032758e6cf1e0a4a1a5e041c2d69
Ayumu Kojima <youki.6230@gmail.com>
2022-04-27 11:53:43 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_08_183615) do
+ActiveRecord::Schema.define(version: 2022_04_25_145249) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false
