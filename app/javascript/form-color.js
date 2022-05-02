885677bfaefad0378d7cd2298b2032c4bf83a75a
Ayumu Kojima <youki.6230@gmail.com>
2022-05-02 14:40:39 +0900

    categoryフォームのカラー変更のjsファイルの編集

--- app/javascript/form-color.js
+++ app/javascript/form-color.js
@@ -1,12 +1,11 @@
 function formColor () {
-  const selector = document.getElementById("category-color-form");
   const colors = document.querySelectorAll(".color");
   const newCategory = document.getElementById("new-category");
   const newCategoryName = document.getElementById("new-category-name");
-  const categoryNameForm = document.getElementById("category-name-form");
-  const categoryColorForm = document.getElementById("category-color-form");
+  const categoryNameForm = document.getElementById("title");
+  const categoryColorForm = document.getElementById("color_id");
   const categoryInputBtn = document.getElementById("category-input-btn");
-  const num = selector.value;
+  const num = categoryColorForm.value;
   const rgb = colors[num].innerHTML;
   newCategory.setAttribute("style", `border-color: rgb${rgb};`);
   newCategoryName.setAttribute("style", `color: rgb${rgb};`);
@@ -14,8 +13,8 @@ function formColor () {
   categoryColorForm.setAttribute("style", `border-color: rgb${rgb};`);
   categoryInputBtn.setAttribute("style", `background-color: rgb${rgb};`);
 
-  selector.addEventListener("change",() => {
-    const num = selector.value;
+  categoryColorForm.addEventListener("change",() => {
+    const num = categoryColorForm.value;
     const rgb = colors[num].innerHTML;
     newCategory.setAttribute("style", `border-color: rgb${rgb};`);
     newCategoryName.setAttribute("style", `color: rgb${rgb};`);
